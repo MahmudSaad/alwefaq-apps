85c05b5b4682f1eff57413af8235fbc4f4b07b61
Mahmudsaad <m.saadara@gmail.com>
2020-08-03 11:54:39 +0200

    alwefaq apps

---
+++ Script/Script.js
@@ -0,0 +1,13 @@
+// Form Move
+VanillaTilt.init(document.querySelector(".test"), {
+    max: 35,
+    speed: 400
+});
+// Move to App Page
+function signin (){
+    window.location = 'main.html'};
+
+    function signout (){
+        window.location = 'index.html'};
+    
+
